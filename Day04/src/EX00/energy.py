363e6a166822da6c7af0ab9dc1d0910990456200
Fuse23 <ivan.brysyakin@gmail.com>
2023-10-16 23:29:07 +0400

    subject day04

---
+++ Day04/src/EX00/energy.py
@@ -0,0 +1,11 @@
+from typing import List, Any
+
+
+def fix_wiring(cabels: List[str | Any],
+               sockets: List[str | Any],
+               plugs: List[str | Any]) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    pass
